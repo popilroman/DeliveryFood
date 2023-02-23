2e41d4699f0f5175ac079c7d3dc040699689d7cc
Popil Roman <popil.roman23@mail.ru>
2023-02-23 15:07:47 +0500

    Подключил СКВ

---
+++ js/restaurant.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    
+    var  shopCartButton = $(".shop-cart"); //Переменная хранит ссылку на класс кнопки
+
+
+    //Функция, которая добавляет и удаляет классы 
+    function openModal () {
+        $(".modal").toggleClass("modal__is-open"); //Находим класс .modal и добавляем к нему класс modal__is-open
+        $("body").toggleClass("scroll-locked"); //Находим тэг body и добавляем к нему класс scroll-locked
+    }
+
+    shopCartButton.on("click", openModal); //Функция срабатывает при клике на содержимое переменной shopCartButton
+    $(".modal__close").on("click", openModal);
+    $(".button__cancel").on("click", openModal);
+
+
+});
+
+new WOW().init(); //Инициализация бибилиотеки WOW.js
