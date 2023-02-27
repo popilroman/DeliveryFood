e927436bd5e5ab7a19c39370b28a5c5a3efba284
Popil Roman <popil.roman23@mail.ru>
2023-02-27 11:56:05 +0500

    Отключил вертикальный скролл при загрузке

--- js/preloader.js
+++ js/preloader.js
@@ -1,11 +1,15 @@
 document.body.onload = function() 
 {
+    const preloader = document.querySelector(".preloader");
+    const bodyTag = document.body;
+
+    bodyTag.classList.add("hidden");
     setTimeout(function()
     {
-        const preloader = document.querySelector(".preloader");
         if (preloader.classList.contains("done") === false)
         {
             preloader.classList.add("done");
+            bodyTag.classList.remove("hidden");
         }
     }, 1000);
 }
