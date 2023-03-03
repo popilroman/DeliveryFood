122a9c1232f0faf2af930114a95bde8bd84a0bbd
Popil Roman <popil.roman23@mail.ru>
2023-03-03 12:04:39 +0500

    Замена JQuery на VanilaJS Измение модального окна корзины

--- js/restaurant.js
+++ js/restaurant.js
@@ -1,17 +1,41 @@
-$(document).ready(function() {
-    
-    var  shopCartButton = $(".shop-cart"); //Переменная хранит ссылку на класс кнопки
+document.addEventListener("DOMContentLoaded", function() 
+{
+    window.addEventListener("load", function() 
+    {
+        const shopCartButton = this.document.querySelector(".shop-cart");
+        const modal = this.document.querySelector(".modal");
+        const modalClose = this.document.getElementById("modal-close");
+        const cancelBtn = this.document.querySelector(".button__cancel");
+        const body = this.document.body;
 
-
-    //Функция, которая добавляет и удаляет классы 
-    function openModal () {
-        $(".modal").toggleClass("modal__is-open"); //Находим класс .modal и добавляем к нему класс modal__is-open
-        $("body").toggleClass("scroll-locked"); //Находим тэг body и добавляем к нему класс scroll-locked
-    }
-
-    shopCartButton.on("click", openModal); //Функция срабатывает при клике на содержимое переменной shopCartButton
-    $(".modal__close").on("click", openModal);
-    $(".button__cancel").on("click", openModal);
-
-
+        shopCartButton.addEventListener("click", openModal);
+        modalClose.addEventListener("click", closeModalByCross);
+        cancelBtn.addEventListener("click", closeModalByBtn);
+        modal.addEventListener("click", modalOutClose);
+        
+        function openModal()
+        {
+            modal.classList.add("modal__is-open");
+            body.style.overflowY = "hidden";
+        }
+        function closeModalByCross()
+        {   
+            modal.classList.remove("modal__is-open"); 
+            body.style.overflowY = "visible";
+        }
+        function closeModalByBtn ()
+        {
+            modal.classList.remove("modal__is-open"); 
+            body.style.overflowY = "visible";   
+        }
+        function modalOutClose(event)
+        {
+            if (event.target === modal)
+            {
+                modal.classList.remove("modal__is-open"); 
+                body.style.overflowY = "visible"; 
+            }
+            
+        }
+    });
 });
